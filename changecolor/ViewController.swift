f11232aa7173dd8f1be3ee59e3451a0ea6763574
youngju <asdadssad@naver.coim>
2018-03-14 15:32:05 +0900

    add lable.text

--- changecolor/ViewController.swift
+++ changecolor/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var flag = 0
 
+    @IBOutlet weak var lblc: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.yellow
     }
-
+    @IBAction func Button(_ sender: Any) {
+        if flag == 0{view.backgroundColor = UIColor.green
+            flag = 1
+            lblc.text = "green"
+        }
+        else if flag == 1{view.backgroundColor = UIColor.yellow
+            flag = 0
+            lblc.text = "yellow"
+        }
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
